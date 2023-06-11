f1d8335a7da95c93e924ab779137353b8c6b8940
Shaya <s.s.bhailal@student.rug.nl>
2023-06-11 11:58:32 +0200

    Started functionality of a set

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-var tablerow = document.getElementsByTagName('tr');
-var tableslots = document.getElementsByTagName('td');
+document.addEventListener("DOMContentLoaded", function() {
+
+var playerTurn = document.querySelector('.player-turn');
+const slots = document.querySelectorAll('.slot');
+
+for (let i = 0; i < 7; i ++){
+    slots[0].addEventListener('click', (e) => {
+        slots[35].style.backgroundColor = 'red';
+    });
+    slots[1].addEventListener('click', (e) => {
+        slots[36].style.backgroundColor = 'red';
+    });
+    slots[2].addEventListener('click', (e) => {
+        slots[37].style.backgroundColor = 'red';
+    });
+    slots[3].addEventListener('click', (e) => {
+        slots[38].style.backgroundColor = 'red';
+    });
+    slots[4].addEventListener('click', (e) => {
+        slots[39].style.backgroundColor = 'red';
+    });
+    slots[5].addEventListener('click', (e) => {
+        slots[40].style.backgroundColor = 'red';
+    });
+    slots[6].addEventListener('click', (e) => {
+        slots[41].style.backgroundColor = 'red';
+    });
+}
+});
 
 
