41c75890503efdb63938ecf41dc258c2d3c88c7d
mijkevandaal <m.a.h.van.daal@student.rug.nl>
2023-06-15 23:50:08 +0200

    add server-side validation

--- index2.php
+++ index2.php
@@ -22,19 +22,25 @@ if (isset($_POST['submit2'])) {
             $namekey = $key;
         }
     }
-    $names[$namekey] = [
-        'id' => 2,
-        'name' => $_POST['name2'],
-    ];
-    $json_file = fopen('data/names.json', 'w');
-    fwrite($json_file, json_encode($names));
-    fclose($json_file);
 
-    // Redirect to game page
-    header("Location: ./game.php");
-    die();
+    $input_name = $_POST['name2'];
+
+    if(is_string($input_name) && strlen($input_name)<11) {
+        $names[$namekey] = [
+            'id' => 2,
+            'name' => $_POST['name2'],
+        ];
+        $json_file = fopen('data/names.json', 'w');
+        fwrite($json_file, json_encode($names));
+        fclose($json_file);
+
+        // Redirect to game page
+        header("Location: ./game.php");
+        die();
+    }
 }
 ?>
+    <script src="js/validatename2.js" charset="utf-8"></script>
 
 
     <header class="intro">Connect Four</header>
@@ -42,13 +48,24 @@ if (isset($_POST['submit2'])) {
     <div class="container">
         <div class="row">
             <div class="col-md-6 enter-name">
-                <form id='user-form' action="index2.php" method="POST">
+                <form id='user-form' class="form-2" action="index2.php" method="POST">
                     <div class="form-group">
                         <label for="inputname">Player 2, enter your name</label>
-                        <input type="text" class="form-control" id="inputname2" name="name2">
+                        <input type="text" class="form-control" id="name2" name="name2">
+                        <?php
+                        if (strlen($input_name)>=11) {
+                            echo "Name cannot be longer than 10 characters";
+                        }
+                        ?>
+                    </div>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
+                    <div class="invalid-feedback">
+                        Please enter a name.
                     </div>
                     <button id="submit" name="submit2" type="submit" class="btn btn-primary">Add player</button>
-                    <script src="main.js" charset="utf-8"></script>
+                    <script src="js/main.js" charset="utf-8"></script>
                 </form>
             </div>
             <div class="col-md-6 explanation">
