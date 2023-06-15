557e2cfe075dd8a3df45b83fdf09dfde2dce921f
mijkevandaal <m.a.h.van.daal@student.rug.nl>
2023-06-15 12:50:42 +0200

    changed game.php

--- game.php
+++ game.php
@@ -12,19 +12,7 @@ include __DIR__ . '/tpl/head.php';
 include __DIR__ . '/tpl/body_start.php';
 ?>
 
-<!doctype html>
-
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Connect Four</title>
-    <link rel="stylesheet" href="css/styles.css">
-    <script src="js/main.js" charset="utf-8"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/js/bootstrap.min.js" integrity="sha512-UR25UO94eTnCVwjbXozyeVd6ZqpaAE9naiEUBK/A+QDbfSTQFhPGj5lOR6d8tsgbBk84Ggb5A3EkjsOgPRPcKA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-</head>
-
+<script src="js/main.js" charset="utf-8"></script>
 
 <div class="pd-40"></div>
     <div class="row">
@@ -54,7 +42,7 @@ include __DIR__ . '/tpl/body_start.php';
 </div>
 
 
-<body>
+
 <div class="player-turn"></div>
 <div id="result"></div>
 <button id="start-button">Start Game</button>
@@ -62,6 +50,8 @@ include __DIR__ . '/tpl/body_start.php';
 
 <p id="inputname">Player < Wins: 0</p>
 <p id="inputgame">Player Two Wins: 0</p>
-</body>
-</html>
+
+<?php
+include __DIR__ . '/tpl/body_end.php';
+?>
 
