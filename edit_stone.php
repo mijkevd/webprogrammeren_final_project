592374053a7f94a159946d9db8984323c319d5e1
Shaya <s.s.bhailal@student.rug.nl>
2023-06-15 16:59:59 +0200

    Update JSON

--- edit_stone.php
+++ edit_stone.php
@@ -3,17 +3,31 @@ if (isset($_POST['submit'])) {
     // Read stones
     $json_file = file_get_contents("data/stones.json");
     $stones = json_decode($json_file, true);
+    $json_names = file_get_contents("data/stones.json");
+    $names = json_decode($json_file, true);
+    $playerturn = 1;
     // Get article key
-    foreach($stones as $key => $value){
-        if ($value['id'] == $_POST['stone_id']){
+    foreach ($stones as $key => $value) {
+        if ($value['id'] == $_POST['stone_id']) {
             $stone_key = $key;
+            // Edit article
+
+            if ($playerturn === 1) {
+                $stones[$stone_key] = [
+                    'id' => $_POST['stone_id'],
+                    'color' => 'yellow',
+                ];
+                $playerturn = 2;
+            }
+            if ($playerturn === 2) {
+                $stones[$stone_key] = [
+                    'id' => $_POST['stone_id'],
+                    'color' => 'red',
+                ];
+                $playerturn = 1;
+            }
         }
     }
-    // Edit article
-    $stones[$stone_key] = [
-        'id' => $_POST['stone_id'],
-        'color' => $_POST['color'],
-    ];
     // Save to external file
     $json_file = fopen('/data/stones.json', 'w');
     fwrite($json_file, json_encode($stones));
--- stones_edit.php
+++ stones_edit.php
@@ -3,22 +3,13 @@ if(isset($_POST['id'])){
     // Read stones
     $json_file = file_get_contents("data/stones.json");
     $stones = json_decode($json_file, true);
-    $player1 = 'yellow';
-    $player2 = 'red';
-    $playerturn = 1;
     // Get stone information
     $stone_id = '';
     $stone_color = '';
     foreach($stones as $key => $value){
-        if($value['id'] == $_POST['id'] && $playerturn == 1){
+        if($value['id'] == $_POST['id']){
             $stone_id = $value['id'];
-            $stone_color = 'yellow';
-            $playerturn = 2;
-            break;
-        } else if ($value['id'] == $_POST['id'] && $playerturn == 2){
-            $stone_id = $value['id'];
-            $stone_color = 'red';
-            $playerturn = 1;
+            $stone_color = $value['color'];
             break;
         }
     }
