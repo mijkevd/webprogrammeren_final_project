dd5b1731440a5accd95e97d744a517635c83abb2
mijkevandaal <m.a.h.van.daal@student.rug.nl>
2023-06-15 16:59:26 +0200

    add modify index.php

--- index.php
+++ index.php
@@ -12,18 +12,42 @@ include __DIR__ . '/tpl/head.php';
 include __DIR__ . '/tpl/body_start.php';
 ?>
 
+<?php
+if (isset($_POST['submit1'])) {
+    $json_file = file_get_contents("data/names.json");
+    $names = json_decode($json_file, true);
+
+    foreach($names as $key => $value){
+        if ($value['id'] === 1){
+            $namekey = $key;
+        }
+    }
+    $names[$namekey] = [
+        'id' => 1,
+        'name' => $_POST['name1'],
+    ];
+    $json_file = fopen('data/names.json', 'w');
+    fwrite($json_file, json_encode($names));
+    fclose($json_file);
+
+    // Redirect to game page
+    header("Location: ./game.php");
+    die();
+}
+?>
+
 
 <header class="intro">Connect Four</header>
 
     <div class="container">
         <div class="row">
             <div class="col-md-6 enter-name">
-                <form id='user-form' method="POST">
+                <form id='user-form' action="index.php" method="POST">
                     <div class="form-group">
-                        <label for="inputname">Enter your name</label>
-                        <input type="text" class="form-control" id="inputname" name="name">
+                        <label for="inputname">Player 1, enter your name</label>
+                        <input type="text" class="form-control" id="inputname1" name="name1">
                     </div>
-                    <button id="submit" type="submit" class="btn btn-primary">Start Game</button>
+                    <button id="submit" name="submit1" type="submit" class="btn btn-primary">Add player</button>
                     <script src="js/main.js" charset="utf-8"></script>
                 </form>
             </div>
@@ -41,6 +65,7 @@ include __DIR__ . '/tpl/body_start.php';
         </div>
     </div>
 
-    <?php
-    include __DIR__ . '/tpl/body_end.php';
-    ?>
+
+<?php
+include __DIR__ . '/tpl/body_end.php';
+?>
