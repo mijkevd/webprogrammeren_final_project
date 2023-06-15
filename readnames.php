a8b4e5ffd45af5ad237fe65ffaacd8609202b6ac
Shaya <s.s.bhailal@student.rug.nl>
2023-06-16 00:25:21 +0200

    Actively switching turns second approach

--- readnames.php
+++ readnames.php
@@ -18,13 +18,4 @@ if (isset($_POST['call_now'])) {
     header('Content-Type: application/json');
     echo json_encode($export_data);
 }
-
-if (isset($_POST['newTurn'])) {
-    $json_file = file_get_contents('playerturn.json');
-    $data = json_decode($json_file, true);
-
-    $data[0]['turn'] = $_POST['newTurn'];
-
-    file_put_contents('turn.json', json_encode($data));
-}
 ?>
