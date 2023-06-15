5c97cb3e8d4d20ed9e55b613dd5571f5606c7051
mijkevandaal <m.a.h.van.daal@student.rug.nl>
2023-06-15 21:30:04 +0200

    add index2.php

--- index2.php
+++ index2.php
@@ -12,18 +12,42 @@ include __DIR__ . '/tpl/head.php';
 include __DIR__ . '/tpl/body_start.php';
 ?>
 
+<?php
+if (isset($_POST['submit2'])) {
+    $json_file = file_get_contents("data/names.json");
+    $names = json_decode($json_file, true);
+
+    foreach($names as $key => $value){
+        if ($value['id'] === 2){
+            $namekey = $key;
+        }
+    }
+    $names[$namekey] = [
+        'id' => 2,
+        'name' => $_POST['name2'],
+    ];
+    $json_file = fopen('data/names.json', 'w');
+    fwrite($json_file, json_encode($names));
+    fclose($json_file);
+
+    // Redirect to game page
+    header("Location: ./game.php");
+    die();
+}
+?>
+
 
     <header class="intro">Connect Four</header>
 
     <div class="container">
         <div class="row">
             <div class="col-md-6 enter-name">
-                <form id='user-form' method="POST">
+                <form id='user-form' action="index2.php" method="POST">
                     <div class="form-group">
                         <label for="inputname">Player 2, enter your name</label>
-                        <input type="text" class="form-control" id="inputname" name="name">
+                        <input type="text" class="form-control" id="inputname2" name="name2">
                     </div>
-                    <button id="submit" type="submit" class="btn btn-primary">Start Game</button>
+                    <button id="submit" name="submit2" type="submit" class="btn btn-primary">Add player</button>
                     <script src="main.js" charset="utf-8"></script>
                 </form>
             </div>
