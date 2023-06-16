d55f21b0543c0919becd5f8fecbbd7582de638df
mijkevandaal <m.a.h.van.daal@student.rug.nl>
2023-06-16 23:23:06 +0200

    change typo

--- tpl/head.php
+++ tpl/head.php
@@ -18,7 +18,7 @@ function p_print($array){
     <!-- Styles -->
     <link rel="preconnect" href="https://cdnjs.cloudflare.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/index.css">
 
     <!-- Scripts -->
